cdcffe393d773a4cf776a97bbb1d876e7d26687d
tala.alshaar@unifr.ch <alshaart@diufrd140.unifr.ch>
2024-12-04 23:49:57 +0100

    set the 1st sequence(T2W) to zero

--- segmentation/predict_2d.py
+++ segmentation/predict_2d.py
@@ -34,6 +34,9 @@ def predict_process(test_path,config,base_dir):
     in_2 = sitk.GetArrayFromImage(in_2).astype(np.float32)
     in_3 = sitk.GetArrayFromImage(in_3).astype(np.float32)
 
+    in_1 = np.copy(in_2)
+    in_1[in_1 > 0] = 0
+
     image = np.stack((in_1,in_2,in_3),axis=0)
 
     with torch.no_grad():

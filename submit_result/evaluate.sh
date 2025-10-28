bec78edd47ddbebbcdd785f07e60fc9cc1e5cae0
tala.alshaar@unifr.ch <alshaart@diufrd200.unifr.ch>
2025-10-28 19:47:52 +0100

    4folds pipeline

---
+++ submit_result/evaluate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#./build.sh
+# change /HOME/alshaart/testdata/ to the directory where weights are 
+# change/HOME/alshaart/testoutput to the directory where the outputs are to be written
+
+
+for i in $(find "/HOME/alshaart/Data/picai_data/images_path/fold0/" -type d -maxdepth 1 -printf '%f '); do 
+    docker run  --gpus='"device=2"' --rm \
+        -v /HOME/alshaart/testdata_ADCavg/:/model/ \
+        -v /HOME/alshaart/testoutput_ADCavg:/output/ \
+        -v /HOME/alshaart/Data/picai_data/images_path/fold0/:/input/ \
+        picai_baseline_unet_processor python3 process.py --caseid $i;
+done

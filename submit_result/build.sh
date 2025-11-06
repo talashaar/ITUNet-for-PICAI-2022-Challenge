235def400e3336110dced9403e08a84b041e894b
tala.alshaar@unifr.ch <alshaart@diufrd200.unifr.ch>
2025-11-06 13:10:45 +0100

    4folds commands pipeline

--- submit_result/build.sh
+++ submit_result/build.sh
@@ -2,6 +2,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker build "$SCRIPTPATH" \
-    -t picai_baseline_unet_processor
+    -t picai_baseline_unet_processor_hbvavg
 
-docker save -o picai_baseline_unet_processor_HBVavg.tar picai_baseline_unet_processor_HBVavg
+docker save -o picai_baseline_unet_processor_hbvavg.tar picai_baseline_unet_processor_hbvavg

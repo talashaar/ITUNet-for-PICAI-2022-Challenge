9984e889880777179341ec8f613d35744653d3d9
tala.alshaar@unifr.ch <alshaart@diufrd200.unifr.ch>
2025-11-14 21:57:52 +0100

    4folds pipeine

--- step_3_prepare_detection_data.py
+++ step_3_prepare_detection_data.py
@@ -34,7 +34,7 @@ def main():
 
     # Perform inference with the classification model
     predict_test5c(
-        weight_path=supervised_weights_dir / 'step1_classification_HBVZero' / 'ckpt/picai/v0/',
+        weight_path=supervised_weights_dir / 'step1_classification_HBVzero_4folds' / 'ckpt/picai/v0/',
         base_dir=preprocessed_dir / 'nnUNet_test_data',
         csv_save_path=workdir / 'test_3c.csv',
     )
@@ -44,7 +44,7 @@ def main():
     outdir = workdir / 'segout/segmentation_result'
     save_npy(
         data_path=data_path,
-        ckpt_path_base=supervised_weights_dir / 'step2_segmentation_HBVZero' / 'ckpt/seg/',
+        ckpt_path_base=supervised_weights_dir / 'step2_segmentation_HBVzero_4folds' / 'ckpt/seg/',
         save_dir_base=workdir / 'segout',
     )
 
--- submit_result/build.sh
+++ submit_result/build.sh
@@ -2,4 +2,6 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker build "$SCRIPTPATH" \
-    -t picai_baseline_unet_processor
+    -t picai_baseline_unet_processor_hbvzero
+
+docker save -o picai_baseline_unet_processor_hbvzero.tar picai_baseline_unet_processor_hbvzero

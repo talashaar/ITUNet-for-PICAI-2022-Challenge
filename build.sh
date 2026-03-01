f12fe13f68dd406a7bf20142253fba79849da79b
tala.alshaar@unifr.ch <alshaart@diufrd200.unifr.ch>
2026-03-01 22:28:05 +0100

    new full

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker build . \
   --tag yukiyaumimi/picai_trainer:latest
 
-docker save -o picai_trainer_latest.tar yukiyaumimi/picai_trainer:latest
+docker save -o picai_trainer_latest_new_full.tar yukiyaumimi/picai_trainer:latest
